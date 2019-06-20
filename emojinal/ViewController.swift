34548b4d8641bb01fe01b9c19ffebb53b8e8e139
sara shelton <saranic314@gmail.com>
2019-06-20 15:19:21 -0500

    complete iteration one

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "mOnKeY bUsInEsS 🙊", message: "go crazy", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+        //needs to have notification
+        //needs to say a message
+        //needs to have options for messages
+    }
 
 }
 
