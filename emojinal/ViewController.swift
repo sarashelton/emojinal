b3bbb2dfd59552261e580882153083b7e304edb1
sara shelton <saranic314@gmail.com>
2019-06-20 15:01:14 -0500

    Initial Storyboard

---
+++ emojinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emojinal
+//
+//  Created by sara shelton on 6/20/19.
+//  Copyright © 2019 sara shelton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
